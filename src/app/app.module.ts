c9f7a664b12ade35d755df173c2fcf2cb48cd6f6
eric <ericceshi1@163.com>
2017-05-03 15:58:30 +0000

    feat: add task detail component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent }  from './app.component';
+import {TaskDetailComponent} from './task-detail.component';
 
 @NgModule({
   imports:      [ BrowserModule ],
-  declarations: [ AppComponent ],
+  declarations: [
+    AppComponent,
+    TaskDetailComponent
+  ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
---
+++ src/app/task-detail.component.ts
@@ -0,0 +1,11 @@
+import {Component} from '@angular/core';
+/**
+ * Created by Mzz on 2017/5/3.
+ */
+@Component({
+  selector: 'task-detail',
+  templateUrl: './taskdetail.component.html'
+})
+export class TaskDetailComponent {
+
+}
