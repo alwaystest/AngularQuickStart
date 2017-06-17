300d4f5c79f448c104cbe9d6dbefccd44f24a781
eric <ericceshi1@163.com>
2017-06-18 01:33:38 +0800

    feature: get actual task name and size

--- src/app/task-detail.component.ts
+++ src/app/task-detail.component.ts
@@ -11,16 +11,59 @@ import {TaskService} from './task.service';
   providers: [TaskService]
 })
 export class TaskDetailComponent {
-  tasks: Task[];
+  tasks: Array<Task>;
 
   constructor(private taskService: TaskService) {
   }
 
   getTasks() {
-    this.taskService.getTasks().then(tasks => this.tasks = tasks);
+    this.taskService.getTasks(this.taskService.getUrl(), Date.now().toString(), this.taskService.getToken())
+      .subscribe(response => {
+        console.log(response.json().result);
+        if (! response.json().result) {
+          return;
+        }
+        for (let item of response.json().result) {
+          let task = new Task();
+          task.name = this.getTaskName(item);
+          task.size = item.totalLength;
+          task.progress = item.completedLength;
+          this.tasks.push(task);
+        }
+      });
   }
 
+  getTaskName(result: any): string {
+    let taskName = 'Undefined';
+    if (!result || result.length === 0) {
+      return taskName;
+    }
+    if (result.bittorrent && result.bittorrent.info) {
+      taskName = result.bittorrent.info.name;
+    }
+    if (!taskName && result.files && result.files.length > 0) {
+      taskName = this.getFileName(result.files[0]);
+    }
+    return taskName;
+  };
+
+  getFileName (file: any) {
+    if (!file) {
+      return 'Undefined';
+    }
+    let path = file.path;
+    if (!path && file.uris && file.uris.length > 0) {
+      path = file.uris[0].uri;
+    }
+    let index = path.lastIndexOf('/');
+    if (index <= 0 || index === path.length) {
+      return path;
+    }
+    return path.substring(index + 1);
+}
+
   ngOnInit() {
+    this.tasks = [];
     this.getTasks();
   }
 }
--- src/app/task.service.ts
+++ src/app/task.service.ts
@@ -4,7 +4,6 @@
 
 import {Injectable} from '@angular/core';
 import {Http, Response} from '@angular/http';
-import {Task} from './Task';
 import {Observable} from 'rxjs/Observable';
 @Injectable()
 export class TaskService {
@@ -12,19 +11,21 @@ export class TaskService {
   constructor(private http: Http) {
   }
 
-  getTasks(): Promise<Task[]> {
-    return Promise.resolve([
-      {name: 'First Task', size: 10, progress: 10},
-      {name: 'Second Task', size: 20, progress: 10},
-      {name: 'Third Task', size: 30, progress: 10}
-    ]);
+  getTasks(url: URL, id: string, token: string): Observable<Response> {
+    let params = {
+      'jsonrpc': '2.0',
+      'method': 'aria2.tellActive',
+      'id': id,
+      'params': ['token:' + token]
+    };
+    return this.http.post(url.toString(), params);
   }
 
   getUrl(): URL {
     return new URL('jsonrpc', 'http://domain:port');
   }
 
-  getToken(): String {
+  getToken(): string {
     return 'token';
   }
 
